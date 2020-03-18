2929ed231f8192648f1fcec406c36dba75448551
shunkang <shunkang1997@gmail.com>
2020-03-18 21:35:18 +0800

    Better counting 1 in binary form

--- count_binary_one.cpp
+++ count_binary_one.cpp
@@ -12,10 +12,22 @@ int countBinaryOne(int number) {
     return count;
 }
 
+int Numberof1(int number) {
+    int count = 0;
+
+    while (number) {
+        count++;
+        number = (number - 1) & number;
+    }
+
+    return count;
+}
+
 int main() {
     int number = 9;
 
     std::cout << "Number of one is " << countBinaryOne(number) << std::endl;
+    std::cout << "Number of one is " << Numberof1(number) << std::endl;
 
     return 0;
 }
