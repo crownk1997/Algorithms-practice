c49ff39bac4c329d1fda4c9066afef894ae13af7
shunkang <shunkang1997@gmail.com>
2020-03-26 00:00:16 +0800

    Fix bug

--- regular_match.cpp
+++ regular_match.cpp
@@ -15,6 +15,12 @@ bool match(const std::string& str, const std::string& pattern) {
 
 bool matchCore(const std::string& str, const std::string& pattern, int index_s, int index_p) {
 
+    if (index_s == str.length() && index_p == pattern.length())
+        return true;
+    
+    if (index_s != str.length() && index_p == pattern.length())
+        return false;
+
     if (pattern[index_p+1] == '*') {
         if (pattern[index_p] == str[index_s] || pattern[index_p] == '.') {
             return matchCore(str, pattern, index_s+1, index_p+2) ||
@@ -30,7 +36,7 @@ bool matchCore(const std::string& str, const std::string& pattern, int index_s, 
 }
 
 int main() {
-    std::string str = "abcaa";
+    std::string str = "abcaba";
     std::string pattern = "abca*";
     bool result = match(str, pattern);
     std::cout << result << std::endl;
